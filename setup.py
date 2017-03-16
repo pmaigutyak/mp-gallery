0d3c6b63015292d37b80ea65da427b9c0fcdd910
Paul Maigutyak <pmaigutyak@ettera.com>
2017-03-16 19:23:40 +0200

    Update version to 1.1

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ except ImportError:
 
 setup(
     name='django-mp-gallery',
-    version='1.0',
+    version='1.1',
     description='Django gallery app',
     long_description=open('README.md').read(),
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url='https://github.com/pmaigutyak/mp-gallery',
-    download_url='https://github.com/pmaigutyak/mp-gallery/archive/1.0.tar.gz',
+    download_url='https://github.com/pmaigutyak/mp-gallery/archive/1.1.tar.gz',
     packages=['gallery'],
     license='MIT',
     install_requires=[
